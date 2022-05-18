77ac64b9cdc96f963033ecf9b99cb7b9083ecc06
Mosaddek Mahdi <34359380+mosaddekmahdi@users.noreply.github.com>
2022-05-18 14:40:21 -0400

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+package test;
+import java.util.*;  
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+	
+	//System.out.println("Hello Universe");
+	//System.out.println("I am Mosy");
+	//System.out.println("gts and kys");
+	
+	Scanner sc= new Scanner(System.in); //System.in is a standard input stream  
+	System.out.print("Enter your name: ");  
+	String str= sc.nextLine();              //reads string  
+	System.out.print("gts and kys "+str);  
+	
+	
+	}
+}
